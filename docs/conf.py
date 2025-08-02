467f8b5216e450b7a663ea307f193360624f48e5
Matin Rafiei <matinrafiei007@gmail.com>
2025-08-02 23:09:58 +0330

    Update conf.py for debugging.

--- docs/conf.py
+++ docs/conf.py
@@ -2,6 +2,8 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../src'))
 
+import sixdman
+print("DEBUG: Imported sixdman version:", getattr(sixdman, "__version__", "unknown"))
 
 
 # Configuration file for the Sphinx documentation builder.
